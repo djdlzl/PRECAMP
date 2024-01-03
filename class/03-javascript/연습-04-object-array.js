693cf8f4086738c2d9e57b5c222e092e88000fbf
chojaewoo <djdlzl93@gmail.com>
2024-01-04 00:33:55 +0900

    cyworld 2

---
+++ class/03-javascript/연습-04-object-array.js
@@ -0,0 +1,14 @@
+let classmate = [
+    { name: "철수", age: 13, school: "다람쥐초등학교"},
+    { name: "영희", age: 8, school: "공룡초등학교"},
+    { name: "훈이", age: 11, school: "거북이초등학교"}
+]
+undefined
+classmate
+(3) [{…}, {…}, {…}]
+classmate.length
+3
+classmate[0].school
+'다람쥐초등학교'
+classmate[2].age
+11
